b322c97dfc0b8fb598cf66b178bf2c2e43280c14
Josip Graho <josip.graho1@gmail.com>
2021-02-23 23:41:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [userInfo, setUserInfo] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading((prevState) => !prevState);
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+      const data = await response.json();
+      setUserInfo(data);
+      setLoading((prevState) => !prevState);
+    };
+
+    fetchData();
+
+    return () => {
+      setUserInfo([]);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Navbar />
+      <div className='container'>
+        <Users loading={loading} users={userInfo} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
