fcb522ab2c249161366b1d2c7080eb3f73774c73
Josip Graho <josip.graho1@gmail.com>
2021-04-24 12:58:00 +0200

    Updates made

--- src/App.js
+++ src/App.js
@@ -1,40 +1,69 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import axios from 'axios';
 import './App.css';
 
-const App = () => {
-  const [userInfo, setUserInfo] = useState([]);
-  const [loading, setLoading] = useState(false);
+class App extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
 
-  useEffect(() => {
-    setLoading((prevState) => !prevState);
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: response.data.items, loading: false });
+  };
 
-    const fetchData = async () => {
-      const response = await fetch(
-        `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-      );
-      const data = await response.json();
-      setUserInfo(data);
-      setLoading((prevState) => !prevState);
-    };
+  clearUsers = () => this.setState({ users: [], loading: false });
 
-    fetchData();
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 3000);
+  };
 
-    return () => {
-      setUserInfo([]);
-    };
-  }, []);
+  render() {
+    const { loading, users, alert } = this.state;
 
-  return (
-    <div className='app'>
-      <Navbar />
-      <div className='container'>
-        <Users loading={loading} users={userInfo} />
-      </div>
-    </div>
-  );
-};
+    return (
+      <Router>
+        <div className='app'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
